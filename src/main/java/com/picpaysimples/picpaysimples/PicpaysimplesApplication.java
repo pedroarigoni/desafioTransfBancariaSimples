e8f503afe302f1c511c798fec4a9a4817deb5a79
pedroargioni <pedroargioni@gmail.com>
2023-11-17 14:39:58 -0300

    PicPay coding challenge

---
+++ src/main/java/com/picpaysimples/picpaysimples/PicpaysimplesApplication.java
@@ -0,0 +1,13 @@
+package com.picpaysimples.picpaysimples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaysimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaysimplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/picpaysimples/picpaysimples/PicpaysimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaysimples.picpaysimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaysimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
